3f51ca5d6c32576137601e0a79d043c4e8c5eed2
eakarpov <eakarpov@yandex.ru>
2019-04-02 01:55:42 +0300

    initial prop

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: "./src/main.ts",
     output: {
         filename: "bundle.js",
+        library: 'graphlabs.core.lib',
+        libraryTarget: 'umd',
         path: __dirname + "/dist"
     },
     devtool: "source-map",
+
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
@@ -20,8 +24,45 @@ module.exports = {
             {
                 enforce: "pre",
                 test: /\.js$/,
+                exclude: '/node_modules',
                 loader: "source-map-loader"
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            localIdentName: "[local]___[hash:base64:5]"
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            modules: true,
+                            localIdentName: "[local]___[hash:base64:5]"
+                        },
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test:/\.(svg|eot|woff2|woff|ttf)$/,
+                loader: 'file-loader',
+            },
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
 };
