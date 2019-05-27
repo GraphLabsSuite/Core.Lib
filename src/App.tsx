13c9950fef5d7db425bff650e95f14f38ebe1f6d
ellina.greek <ellina.ahmetova@mail.ru>
2019-05-27 14:58:12 +0300

    Some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import './App.css';
 import {Matrix} from "./Matrix";
 
 class App extends Component {
-  render() {
-    return (
-      <Matrix
-        rows={2}
-        readonly
-        columns={3}
-        handler={(val) => console.log(val)}
-        initial={[[1,2],[2,3], [3,4]]}
-      />
-    );
-  }
+    render() {
+        return (
+            <Matrix
+                rows={2}
+                readonly
+                columns={3}
+                handler={(val) => console.log(val)}
+                initial={[[1,2],[2,3], [3,4]]}
+            />
+        );
+    }
 }
 
 export default App;
