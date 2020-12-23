e368ddad190586fb8d8a1f4c7c89be69c2e7ab9a
ayarabaeva <60610141+ayarabaeva@users.noreply.github.com>
2020-12-23 23:04:54 +0300

    Update MatrixCell.tsx
    Add new flag  matrixFilling and method handlerForFilling for it. Fixed error for componentWillReceiveProps method

--- src/Matrix/MatrixCell.tsx
+++ src/Matrix/MatrixCell.tsx
@@ -1,5 +1,7 @@
-import {Component, ReactNode} from 'react';
+import {Component, ReactNode, ChangeEvent } from 'react';
 import * as React from 'react';
+
+
 
 interface State {
     value: number;
@@ -9,22 +11,43 @@ export interface IMatrixCell {
     get: (e: number) => void;
     readonly: boolean;
     defaultValue: number;
+    matrixFilling?: boolean;
 }
 
 export class MatrixCell extends Component<IMatrixCell, State> {
 
+
     constructor(props: IMatrixCell) {
         super(props);
         this.state = {
-            value: 0,
+            value: 0
         };
         this.handler = this.handler.bind(this);
+        this.handlerForFilling = this.handlerForFilling.bind(this);
+    }
+
+   handlerForFilling(val: React.ChangeEvent<HTMLInputElement>){
+
+        if (this.props.readonly == true) {
+
+        } else {
+            this.setState(
+                {
+                    value: +val.target.value,
+                },
+
+                () => {
+                    this.props.get(this.state.value);
+                });
+        }
     }
 
     handler() {
         if (this.props.readonly == true) {
 
-        } else {
+        }
+        else {
+
             this.setState(
                 {
                     value: this.state.value ? 0 : 1,
@@ -43,20 +66,36 @@ export class MatrixCell extends Component<IMatrixCell, State> {
         );
     }
 
-    componentWillReceiveProps(nextProps: IMatrixCell): void {
+   componentWillReceiveProps(nextProps: IMatrixCell): void {
         if (nextProps.defaultValue !== this.props.defaultValue) {
             this.setState(
                 {
                     value: nextProps.defaultValue,
+                },
+                () => {
+                    this.props.get(nextProps.defaultValue);
                 }
             );
         }
     }
 
+
     render(): ReactNode {
-        return (
-            <div style={{border: '1px double black', background: 'white', padding: '5px'}} onClick={this.handler}>
-                {this.state.value}
-            </div>);
+        if (this.props.matrixFilling == true)
+        {
+            return (
+                <div>
+                    <input type="text" id={'cell'} name={'myCell'} size={1} style={{border: '1px double black',
+                        background: 'white', padding: '5px', textAlign: 'center'}} onChange={this.handlerForFilling}/>
+                </div>);
+
+        }
+        else {
+          return (
+               <div  style={{border: '1px double black', background: 'white', padding: '5px'}} onClick={this.handler}>
+                    {this.state.value}
+                </div>);
+       }
     }
 }
+
